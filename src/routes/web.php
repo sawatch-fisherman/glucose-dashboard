3189b16047302588d09c9885500facfe0e442117
sawatch-fisherman <sawatch.fisherman@gmail.com>
2024-07-07 18:14:40 +0900

    日本語化ライブラリ(breezejp)をインストールした

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,3 +25,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Language Switcher Route 言語切替用ルートだよ
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+
+    return redirect()->back();
+});
